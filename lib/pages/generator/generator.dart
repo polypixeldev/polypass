61b1393a0fbcd7db25390800fe50d5fa33eef5f7
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2023-07-01 13:16:55 -0400

    feat: make random password generator responsive

--- lib/pages/generator/generator.dart
+++ lib/pages/generator/generator.dart
@@ -15,113 +15,141 @@ class Generator extends StatelessWidget {
   Widget build(BuildContext context) {
     return AppWrapper(
         backButton: true,
-        child: BlocProvider(
-          create: (context) => GeneratorBloc(),
-          child: BlocBuilder<GeneratorBloc, GeneratorState>(
-              builder: (context, state) {
-            return Center(
-                child: Container(
-              padding: const EdgeInsets.all(10),
-              margin: const EdgeInsets.symmetric(horizontal: 75),
-              decoration: BoxDecoration(
-                  color: Theme.of(context).cardColor,
-                  borderRadius: BorderRadius.circular(10)),
-              child: Column(mainAxisSize: MainAxisSize.min, children: [
-                Text('Random Password Generator',
-                    style: Theme.of(context).textTheme.titleMedium),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 5),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      Flexible(
-                          flex: 10,
-                          fit: FlexFit.tight,
-                          child: Container(
-                              decoration: BoxDecoration(
-                                color: Theme.of(context).colorScheme.secondary,
-                                borderRadius: BorderRadius.circular(3),
-                              ),
-                              padding: const EdgeInsets.all(5),
-                              child: Text(state.generatedPassword,
-                                  style: TextStyle(
-                                      fontSize: Theme.of(context)
-                                          .textTheme
-                                          .bodyMedium!
-                                          .fontSize,
-                                      fontFamily: 'monospace',
-                                      color: Colors.black)))),
-                      IconButton(
-                        icon: const Icon(Icons.refresh),
-                        tooltip: 'Regenerate password',
-                        onPressed: () {
-                          context
-                              .read<GeneratorBloc>()
-                              .add(const GeneratorEvent.regeneratePassword());
-                        },
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.copy),
-                        tooltip: 'Copy password',
-                        onPressed: () {
-                          Clipboard.setData(
-                              ClipboardData(text: state.generatedPassword));
-
-                          final appSettingsBloc =
-                              context.read<AppSettingsBloc>();
-                          final seconds = appSettingsBloc.state.settings
-                              .defaultVaultSettings.clipboardClearSeconds;
-
-                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                            content: Text(
-                                'Copied password to clipboard - it will be cleared in $seconds seconds'),
-                          ));
-                          final activityBloc = context.read<ActivityBloc>();
-                          activityBloc.add(const ActivityEvent.copied(true));
-                          Future.delayed(Duration(seconds: seconds)).then((v) {
-                            Clipboard.setData(const ClipboardData(text: ''));
-                            activityBloc.add(const ActivityEvent.copied(false));
-                          });
-                        },
-                      )
-                    ],
-                  ),
-                ),
-                Column(mainAxisSize: MainAxisSize.min, children: [
-                  Row(
-                    children: [
-                      Text('Length: ',
-                          style: Theme.of(context).textTheme.bodyMedium),
-                      Flexible(
-                          flex: 10,
-                          child: Slider(
-                              min: 1,
-                              max: 50,
-                              divisions: 50,
-                              value: state.length.toDouble(),
-                              activeColor:
-                                  Theme.of(context).colorScheme.tertiary,
-                              onChanged: (val) {
+        child: Center(
+          child: SingleChildScrollView(
+            child: BlocProvider(
+              create: (context) => GeneratorBloc(),
+              child: BlocBuilder<GeneratorBloc, GeneratorState>(
+                  builder: (context, state) {
+                return Center(
+                    child: Container(
+                  padding: const EdgeInsets.all(10),
+                  margin: const EdgeInsets.symmetric(horizontal: 50),
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).cardColor,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Column(mainAxisSize: MainAxisSize.min, children: [
+                    Text('Random Password Generator',
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.titleMedium),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 5),
+                      child: LayoutBuilder(builder: (context, constraints) {
+                        return Wrap(
+                          direction: Axis.horizontal,
+                          alignment: WrapAlignment.center,
+                          children: [
+                            Container(
+                                decoration: BoxDecoration(
+                                  color:
+                                      Theme.of(context).colorScheme.secondary,
+                                  borderRadius: BorderRadius.circular(3),
+                                ),
+                                padding: const EdgeInsets.all(5),
+                                constraints:
+                                    const BoxConstraints(minWidth: 750),
+                                child: Text(state.generatedPassword,
+                                    textAlign: constraints.maxWidth < 500
+                                        ? TextAlign.center
+                                        : TextAlign.left,
+                                    style: TextStyle(
+                                        fontSize: Theme.of(context)
+                                            .textTheme
+                                            .bodyMedium!
+                                            .fontSize,
+                                        fontFamily: 'monospace',
+                                        color: Colors.black))),
+                            IconButton(
+                              icon: const Icon(Icons.refresh),
+                              tooltip: 'Regenerate password',
+                              onPressed: () {
                                 context.read<GeneratorBloc>().add(
-                                    GeneratorEvent.lengthChanged(val.toInt()));
-                              })),
-                      SizedBox(
-                        width: 150,
-                        child: SpinBox(
-                            textStyle: Theme.of(context).textTheme.bodyMedium,
-                            min: 1,
-                            max: 50,
-                            value: state.length.toDouble(),
-                            onChanged: (val) {
-                              context.read<GeneratorBloc>().add(
-                                  GeneratorEvent.lengthChanged(val.toInt()));
-                            }),
-                      ),
-                    ],
-                  ),
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
+                                    const GeneratorEvent.regeneratePassword());
+                              },
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.copy),
+                              tooltip: 'Copy password',
+                              onPressed: () {
+                                Clipboard.setData(ClipboardData(
+                                    text: state.generatedPassword));
+
+                                final appSettingsBloc =
+                                    context.read<AppSettingsBloc>();
+                                final seconds = appSettingsBloc.state.settings
+                                    .defaultVaultSettings.clipboardClearSeconds;
+
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(SnackBar(
+                                  content: Text(
+                                      'Copied password to clipboard - it will be cleared in $seconds seconds'),
+                                ));
+                                final activityBloc =
+                                    context.read<ActivityBloc>();
+                                activityBloc
+                                    .add(const ActivityEvent.copied(true));
+                                Future.delayed(Duration(seconds: seconds))
+                                    .then((v) {
+                                  Clipboard.setData(
+                                      const ClipboardData(text: ''));
+                                  activityBloc
+                                      .add(const ActivityEvent.copied(false));
+                                });
+                              },
+                            )
+                          ],
+                        );
+                      }),
+                    ),
+                    Column(mainAxisSize: MainAxisSize.min, children: [
+                      LayoutBuilder(builder: (context, constraints) {
+                        return Flex(
+                          direction: constraints.maxWidth < 500
+                              ? Axis.vertical
+                              : Axis.horizontal,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 5),
+                              child: Text('Length: ',
+                                  style:
+                                      Theme.of(context).textTheme.bodyMedium),
+                            ),
+                            constraints.maxWidth < 500
+                                ? Container()
+                                : Flexible(
+                                    flex: 10,
+                                    child: Slider(
+                                        min: 1,
+                                        max: 50,
+                                        divisions: 50,
+                                        value: state.length.toDouble(),
+                                        activeColor: Theme.of(context)
+                                            .colorScheme
+                                            .tertiary,
+                                        onChanged: (val) {
+                                          context.read<GeneratorBloc>().add(
+                                              GeneratorEvent.lengthChanged(
+                                                  val.toInt()));
+                                        })),
+                            SizedBox(
+                              width: 130,
+                              child: SpinBox(
+                                  textStyle:
+                                      Theme.of(context).textTheme.bodyMedium,
+                                  min: 1,
+                                  max: 50,
+                                  value: state.length.toDouble(),
+                                  onChanged: (val) {
+                                    context.read<GeneratorBloc>().add(
+                                        GeneratorEvent.lengthChanged(
+                                            val.toInt()));
+                                  }),
+                            ),
+                          ],
+                        );
+                      }),
+                      Wrap(alignment: WrapAlignment.spaceEvenly, children: [
                         GeneratorGroup(
                             name: 'Lowercase',
                             value: state.lowercase,
@@ -153,10 +181,12 @@ class Generator extends StatelessWidget {
                                   GeneratorEvent.symbolsChanged(val ?? false));
                             }),
                       ]),
-                ])
-              ]),
-            ));
-          }),
+                    ])
+                  ]),
+                ));
+              }),
+            ),
+          ),
         ));
   }
 }
